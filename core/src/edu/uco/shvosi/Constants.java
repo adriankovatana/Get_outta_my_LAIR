5bd977181d5619bc09416c5f5b0019666f4adc9b
CaryStringfield <captainclark@cox.net>
2015-10-20 17:21:32 -0500

    changed scren size

--- core/src/edu/uco/shvosi/Constants.java
+++ core/src/edu/uco/shvosi/Constants.java
@@ -2,8 +2,8 @@ package edu.uco.shvosi;
 
 public class Constants {
     public static final int TILEDIMENSION = 100;
-    public static final int SCREENWIDTH = 800;
-    public static final int SCREENHEIGHT = 450;
+    public static final int SCREENWIDTH = 1024;
+    public static final int SCREENHEIGHT = 576;
     public static final float MOVEACTIONDURATION = 0.5f;
     public static float MASTERVOLUME = 0.1f;
     
